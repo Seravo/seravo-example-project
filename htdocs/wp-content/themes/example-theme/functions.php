9e5514bd29e57251238265d042ce1121b9713fad
Sami Keijonen <samik@seravo.com>
2025-02-06 15:51:37 +0200

    Remove all extra files
    Example theme started out from the 2025 theme but most of the files are unnecessary.
    That's why most of the files are removed. This theme does not need to look fancy.

--- htdocs/wp-content/themes/example-theme/functions.php
+++ htdocs/wp-content/themes/example-theme/functions.php
@@ -1,18 +1,13 @@
 <?php
 /**
- * Twenty Twenty-Five functions and definitions.
+ * Example theme functions and definitions.
  *
  * @link https://developer.wordpress.org/themes/basics/theme-functions/
  *
- * @package WordPress
- * @subpackage Twenty_Twenty_Five
- * @since Twenty Twenty-Five 1.0
  */
 
 /**
  * Enqueues editor-style.css in the editors.
- *
- * @since Twenty Twenty-Five 1.0
  *
  * @return void
  */
@@ -23,8 +18,6 @@ add_action( 'after_setup_theme', 'example_theme_editor_style' );
 
 /**
  * Enqueues styles and scripts on the front-end.
- *
- * @since Twenty Twenty-Five 1.0
  *
  * @return void
  */
@@ -59,101 +52,3 @@ function example_theme_enqueue_styles() {
 	);
 }
 add_action( 'wp_enqueue_scripts', 'example_theme_enqueue_styles' );
-
-// Registers custom block styles.
-if ( ! function_exists( 'example_theme_block_styles' ) ) :
-	/**
-	 * Registers custom block styles.
-	 *
-	 * @since Twenty Twenty-Five 1.0
-	 *
-	 * @return void
-	 */
-	function example_theme_block_styles() {
-		register_block_style(
-			'core/list',
-			array(
-				'name'         => 'checkmark-list',
-				'label'        => __( 'Checkmark', 'twentytwentyfive' ),
-				'inline_style' => '
-				ul.is-style-checkmark-list {
-					list-style-type: "\2713";
-				}
-
-				ul.is-style-checkmark-list li {
-					padding-inline-start: 1ch;
-				}',
-			)
-		);
-	}
-endif;
-add_action( 'init', 'example_theme_block_styles' );
-
-// Registers pattern categories.
-if ( ! function_exists( 'example_theme_pattern_categories' ) ) :
-	/**
-	 * Registers pattern categories.
-	 *
-	 * @since Twenty Twenty-Five 1.0
-	 *
-	 * @return void
-	 */
-	function example_theme_pattern_categories() {
-
-		register_block_pattern_category(
-			'example_theme_page',
-			array(
-				'label'       => __( 'Pages', 'twentytwentyfive' ),
-				'description' => __( 'A collection of full page layouts.', 'twentytwentyfive' ),
-			)
-		);
-
-		register_block_pattern_category(
-			'example_theme_post-format',
-			array(
-				'label'       => __( 'Post formats', 'twentytwentyfive' ),
-				'description' => __( 'A collection of post format patterns.', 'twentytwentyfive' ),
-			)
-		);
-	}
-endif;
-add_action( 'init', 'example_theme_pattern_categories' );
-
-// Registers block binding sources.
-if ( ! function_exists( 'example_theme_register_block_bindings' ) ) :
-	/**
-	 * Registers the post format block binding source.
-	 *
-	 * @since Twenty Twenty-Five 1.0
-	 *
-	 * @return void
-	 */
-	function example_theme_register_block_bindings() {
-		register_block_bindings_source(
-			'twentytwentyfive/format',
-			array(
-				'label'              => _x( 'Post format name', 'Label for the block binding placeholder in the editor', 'twentytwentyfive' ),
-				'get_value_callback' => 'example_theme_format_binding',
-			)
-		);
-	}
-endif;
-add_action( 'init', 'example_theme_register_block_bindings' );
-
-// Registers block binding callback function for the post format name.
-if ( ! function_exists( 'example_theme_format_binding' ) ) :
-	/**
-	 * Callback function for the post format name block binding source.
-	 *
-	 * @since Twenty Twenty-Five 1.0
-	 *
-	 * @return string|void Post format name, or nothing if the format is 'standard'.
-	 */
-	function example_theme_format_binding() {
-		$post_format_slug = get_post_format();
-
-		if ( $post_format_slug && 'standard' !== $post_format_slug ) {
-			return get_post_format_string( $post_format_slug );
-		}
-	}
-endif;
